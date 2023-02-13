0a15ed5557bb048edb5783bfe63c284fb71ad59c
praiseojarikre <praiseojarikre@gmail.com>
2023-02-13 09:30:48 +0100

    minor chnges

--- src/componets/App.js
+++ src/componets/App.js
@@ -13,7 +13,7 @@ function App() {
   return (
    
       <div className="App">
-<img src={img} alt="" className='imgs'/>
+<img src={img} alt="" className='imgs'/ >
 <Header/>
     </div>
   );
--- src/componets/db.js
+++
@@ -1,4 +0,0 @@
-export const Year = [
-    {id:1, name:'Annual Plan', price:'$59.99',},
-    {id:2, name:'Monthly Plan', title:'$5.00',},
-]
